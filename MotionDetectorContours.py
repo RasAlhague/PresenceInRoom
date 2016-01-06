dca99f82717f1f9d6d892a48e76c44167acecce9
RasAlhague <alexgr.dev@gmail.com>
2016-01-06 17:30:55 +0200

    HotFix

--- MotionDetectorContours.py
+++ MotionDetectorContours.py
@@ -65,7 +65,7 @@ class MotionDetectorAdaptative():
             self.processImage(currentframe)  # Process the image
 
             if not self.isRecording:
-                if self.somethingHasMoved(currentframe):
+                if self.somethingHasMoved():
                     self.trigger_time = instant  # Update the trigger_time
                     if instant > started + 10:  # Wait 5 second after the webcam start for luminosity adjusting etc..
                         # print "Something is moving !"
