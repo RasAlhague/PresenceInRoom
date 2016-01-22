ae982b63e9b745aaa9a905cf49876b5a7d24ef18
RasAlhague <alexgr.dev@gmail.com>
2016-01-22 23:00:38 +0200

    Add exception handling

--- OpenCVRoutine.py
+++ OpenCVRoutine.py
@@ -25,36 +25,40 @@ class OpenCVRoutine(Thread):
         self.opencv_routine()
 
     def opencv_routine(self):
-        cap = cv2.VideoCapture(capture_url)
-        while True:
-            # Capture frame-by-frame
-            ret, bgr_frame = cap.read()
+        try:
+            cap = cv2.VideoCapture(capture_url)
+            while True:
+                # Capture frame-by-frame
+                ret, bgr_frame = cap.read()
 
-            # Choose color space
-            gray_frame = cv2.cvtColor(bgr_frame, cv2.COLOR_BGR2GRAY)
-            equalized_frame = cv2.equalizeHist(gray_frame)
+                # Choose color space
+                gray_frame = cv2.cvtColor(bgr_frame, cv2.COLOR_BGR2GRAY)
+                equalized_frame = cv2.equalizeHist(gray_frame)
 
-            self.frame_callback(equalized_frame)
+                Thread(target=self.frame_callback, args=(equalized_frame,)).start()
 
-            # Display the resulting frame
-            if show_preview:
-                # cv2.imshow('gray_frame', gray_frame)
-                cv2.imshow('equalization', equalized_frame)
+                # Display the resulting frame
+                if show_preview:
+                    # cv2.imshow('gray_frame', gray_frame)
+                    cv2.imshow('equalization', equalized_frame)
 
-            key = cv2.waitKey(1) & 0xFF
-            if key == ord('q'):
-                break
+                key = cv2.waitKey(1) & 0xFF
+                if key == ord('q'):
+                    break
 
-            if key == ord('2') or record_mode == 2:
-                Image.fromarray(bgr_frame).save(
-                        learning_set_path + absence_prefix + "_" + str(datetime.now().time()) + ".jpg",
-                        "JPEG")
+                if key == ord('2') or record_mode == 2:
+                    Image.fromarray(bgr_frame).save(
+                            learning_set_path + absence_prefix + "_" + str(datetime.now().time()) + ".jpg",
+                            "JPEG")
 
-            if key == ord('1') or record_mode == 1:
-                Image.fromarray(bgr_frame).save(
-                        learning_set_path + presence_prefix + "_" + str(datetime.now().time()) + ".jpg",
-                        "JPEG")
+                if key == ord('1') or record_mode == 1:
+                    Image.fromarray(bgr_frame).save(
+                            learning_set_path + presence_prefix + "_" + str(datetime.now().time()) + ".jpg",
+                            "JPEG")
 
-        # When everything done, release the capture
-        cap.release()
-        cv2.destroyAllWindows()
+            # When everything done, release the capture
+            cap.release()
+            cv2.destroyAllWindows()
+        except Exception as e:
+            print(e)
+            self.opencv_routine()
