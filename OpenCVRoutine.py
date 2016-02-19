02382d35d51885bbe0a3d4f2664bd7a3fb5fe0a3
RasAlhague <alexgr.dev@gmail.com>
2016-02-19 21:50:44 +0200

    Add stop_routine func

--- OpenCVRoutine.py
+++ OpenCVRoutine.py
@@ -13,6 +13,7 @@ class OpenCVRoutine(Thread):
         super(OpenCVRoutine, self).__init__()
 
         self.frame_queue = frame_queue
+        self.stop_routine = False
 
         self.setDaemon(True)
         self.start()
@@ -20,14 +21,17 @@ class OpenCVRoutine(Thread):
     def run(self):
         self.opencv_routine()
 
+    def stop_routine(self):
+        self.stop_routine = True
+
     def opencv_routine(self):
         try:
             cap = cv2.VideoCapture(capture_url)
             import threading
             is_main_thread_active = lambda: any(
-                    (i.name == "MainThread") and i.is_alive() for i in threading.enumerate())
+                (i.name == "MainThread") and i.is_alive() for i in threading.enumerate())
 
-            while is_main_thread_active():
+            while is_main_thread_active() or not self.stop_routine:
                 # Capture frame-by-frame
                 ret, bgr_frame = cap.read()
 
@@ -48,21 +52,19 @@ class OpenCVRoutine(Thread):
 
                 if key == ord('2') or OpenCVRoutine.record_mode == 2:
                     Image.fromarray(bgr_frame).save(
-                            learning_set_path + absence_prefix + "_" + str(datetime.now().time()) + ".jpg",
-                            "JPEG")
+                        learning_set_path + absence_prefix + "_" + str(datetime.now().time()) + ".jpg", "JPEG")
 
                 if key == ord('1') or OpenCVRoutine.record_mode == 1:
                     Image.fromarray(bgr_frame).save(
-                            learning_set_path + presence_prefix + "_" + str(datetime.now().time()) + ".jpg",
-                            "JPEG")
-
-        except Exception as e:
-            print e
-            print 'Restarting cv routine'
+                        learning_set_path + presence_prefix + "_" + str(datetime.now().time()) + ".jpg", "JPEG")
 
             # When everything done, release the capture
             if cap:
                 cap.release()
             cv2.destroyAllWindows()
+
+        except Exception as e:
+            print e
+            print 'Restarting cv routine'
 
             OpenCVRoutine(self.frame_queue)
