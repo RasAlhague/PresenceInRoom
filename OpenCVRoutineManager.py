5a569a3a6482e62c371e48a73b96e6a249355ad9
RasAlhague <alexgr.dev@gmail.com>
2016-02-23 11:30:24 +0200

    Fix time issue

--- OpenCVRoutineManager.py
+++ OpenCVRoutineManager.py
@@ -35,22 +35,36 @@ class OpenCVRoutineManager(Thread):
         if self.open_cv_routine:
             self.open_cv_routine.stop_routine()
 
-    def open_cv_managing_routine(self):
+    def _update_datetimes(self):
         format = self.schedule['format']
         start_opencv_routine_from = datetime.datetime.strptime(self.schedule['start_opencv_routine_from'],
                                                                format).time()
         stop_opencv_routine_at = datetime.datetime.strptime(self.schedule['stop_opencv_routine_at'], format).time()
-        while self.is_running:
-            now_date = datetime.datetime.now().date()
-            start_dt = datetime.datetime.combine(now_date, start_opencv_routine_from)
-            stop_dt = datetime.datetime.combine(now_date, stop_opencv_routine_at)
-            stop_dt += datetime.timedelta(days=1)
 
-            if start_dt <= datetime.datetime.now() <= stop_dt:
+        now_datetime = datetime.datetime.now()
+        start_dt = datetime.datetime.combine(now_datetime.date(), start_opencv_routine_from)
+        stop_dt = datetime.datetime.combine(now_datetime.date(), stop_opencv_routine_at)
+        stop_dt += datetime.timedelta(days=1)
+
+        if now_datetime.time() < stop_opencv_routine_at:
+            start_dt += datetime.timedelta(days=-1)
+            stop_dt += datetime.timedelta(days=-1)
+
+        return start_dt, stop_dt
+
+    def open_cv_managing_routine(self):
+        start_dt, stop_dt = self._update_datetimes()
+
+        while self.is_running:
+            now = datetime.datetime.now()
+            if start_dt <= now <= stop_dt:
                 if self.open_cv_routine is None:
                     self.open_cv_routine = OpenCVRoutine(self.frame_queue)
             elif self.open_cv_routine is not None:
                 self.open_cv_routine.stop_routine()
                 self.open_cv_routine = None
+
+            if now > stop_dt:
+                start_dt, stop_dt = self._update_datetimes()
 
             time.sleep(1)
