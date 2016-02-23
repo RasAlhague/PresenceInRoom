c36122f67876a3c67758a594aac1f626ba2050b5
RasAlhague <alexgr.dev@gmail.com>
2016-02-23 20:57:07 +0200

    Update LS

--- WebServer.py
+++ WebServer.py
@@ -6,7 +6,7 @@ from OpenCVRoutine import OpenCVRoutine
 def run():
     app = Flask(__name__)
 
-    @app.route('/mode/set/<int:rm>', methods=['POST'])
+    @app.route('/recordmode/<int:rm>', methods=['POST'])
     def post(rm):
         OpenCVRoutine.record_mode = rm
         return {'record_mode': rm}
