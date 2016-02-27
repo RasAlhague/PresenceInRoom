9bdea7689f945c15bdb7b7ce616a25936a1ad57a
RasAlhague <alexgr.dev@gmail.com>
2016-02-27 11:53:25 +0200

    Add local var

--- WebServer.py
+++ WebServer.py
@@ -2,13 +2,19 @@ from flask import Flask
 
 from OpenCVRoutine import OpenCVRoutine
 
+record_mode = 0
+
 
 def run():
     app = Flask(__name__)
 
     @app.route('/recordmode/<int:rm>', methods=['POST'])
     def post(rm):
+        global record_mode
+
         OpenCVRoutine.record_mode = rm
+        record_mode = rm
+
         return {'record_mode': rm}
 
     app.run(host='0.0.0.0')
